6b05865fca183b7837a9e9e501107ad9dee66c0e
Pragati Moharana <pragatimoharana3@gmail.com>
2025-02-19 23:57:39 +0530

    second commit

--- script.js
+++ script.js
@@ -16,12 +16,12 @@
 let color = document.querySelectorAll('li');
 
 
-// color.forEach((color)=>(color.addEventListener('click',()=>{
-//     document.body.style.backgroundColor=color.id;
-// })));
+color.forEach((color)=>(color.addEventListener('click',()=>{
+    document.body.style.backgroundColor=color.id;
+})));
 
 //there is a call back funcction in forEach and inside that function we added evet listener which consists of
 //one event and another function
 
 
-color.forEach((color)=>color.onclick=()=>{console.log(color.id)});
+// color.forEach((color)=>color.onclick=()=>{console.log(color.id)});
