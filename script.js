4c5eebf2728265b10a466a5dc854d01717f9f56a
Pragati Moharana <pragatimoharana3@gmail.com>
2025-02-19 23:52:28 +0530

    first commit of ColorChangeUsingDOM

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+// let green= document.getElementById("green");
+// let yellow= document.getElementById("yellow");
+// let red= document.getElementById("red");
+// let sky = document.getElementById("sky");
+// let black = document.getElementById("black");
+
+
+// green.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "green";
+// });
+
+
+
+let color = document.querySelectorAll('li');
+
+
+// color.forEach((color)=>(color.addEventListener('click',()=>{
+//     document.body.style.backgroundColor=color.id;
+// })));
+
+//there is a call back funcction in forEach and inside that function we added evet listener which consists of
+//one event and another function
+
+
+color.forEach((color)=>color.onclick=()=>{console.log(color.id)});
